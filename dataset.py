now using VIT; also fixed bug in disc loss calculation

dataset.py
@@ -16,13 +16,14 @@ class Dataset:
 
 
 class DatasetLoaderLite(Dataset):
-  def __init__(self, train: bool, root: str, batch_size: int, shuffle: bool):
+  def __init__(self, train: bool, root: str, batch_size: int, shuffle: bool, patch_size: int):
     self.ds = torchvision.datasets.CIFAR10(root=root, train=train, download=False)
     self.batch_size = batch_size
     self.indices = list(range(len(self.ds)))
     if shuffle: random.shuffle(self.indices)
 
     self.to_tensor = ToTensor()
+    self.patch_size = patch_size
     self.reset()
 
   def reset(self):
@@ -33,8 +34,24 @@ class DatasetLoaderLite(Dataset):
     self.curr_idx = (self.curr_idx + self.batch_size) % len(self.ds)
     
     imgs = []
+    n_rows_list, n_cols_list = [], []
     for idx in next_indices:
       img, _ = self.ds[idx]
-      imgs.append(self.to_tensor(img).flatten())
+      img_tensor = self.to_tensor(img)
+      n_channels, n_rows, n_cols = img_tensor.size()
+      img_tensor = img_tensor.view(n_channels, n_rows//self.patch_size, self.patch_size, n_cols//self.patch_size, self.patch_size)
+      img_tensor = img_tensor.permute(1, 3, 0, 2, 4).flatten(2)
+      n_rows_list.append(img_tensor.size(0))
+      n_cols_list.append(img_tensor.size(1))
+      img_tensor = img_tensor.flatten(0, 1)  # (n_patches, patch_size*patch_size*n_channels)
+      imgs.append(img_tensor)
 
-    return torch.stack(imgs)
+    # assert all values in n_rows_list and n_cols_list are the same
+    assert all([n_rows_list[0] == n_rows for n_rows in n_rows_list])
+    assert all([n_cols_list[0] == n_cols for n_cols in n_cols_list])
+
+    return {
+      'imgs': torch.stack(imgs),
+      'n_rows': n_rows_list[0],
+      'n_cols': n_cols_list[0],
+    }

engine.py
@@ -11,7 +11,10 @@ def run(train_ds, test_ds, vqgan, disc, vqgan_opt, disc_opt, N_STEPS, device):
   vqgan_opt.zero_grad()
   disc_opt.zero_grad()
 
-  test_samples = test_ds.next_batch().to(device)
+  test_samples = test_ds.next_batch()
+  test_images = test_samples['imgs'].to(device)
+  test_n_rows = test_samples['n_rows']
+  test_n_cols = test_samples['n_cols']
 
   print("Training for steps:", N_STEPS)
   vqgan.train()
@@ -20,7 +23,10 @@ def run(train_ds, test_ds, vqgan, disc, vqgan_opt, disc_opt, N_STEPS, device):
   start = time.monotonic()
   for i in range(N_STEPS):
 
-    batch = train_ds.next_batch().to(device)
+    batch = train_ds.next_batch()
+    images = batch['imgs'].to(device)
+    n_rows = batch['n_rows']
+    n_cols = batch['n_cols']
 
     # train discriminator
     for params in vqgan.parameters(): params.requires_grad = False
@@ -28,10 +34,10 @@ def run(train_ds, test_ds, vqgan, disc, vqgan_opt, disc_opt, N_STEPS, device):
 
     vqgan_opt.zero_grad()
     disc_opt.zero_grad()
-    real_img_disc_out = disc(batch).sigmoid()
+    real_img_disc_out = disc(images)
     
-    with torch.no_grad(): fake_imgs, _ = vqgan(batch)
-    fake_disc_out = disc(fake_imgs).sigmoid()
+    with torch.no_grad(): fake_imgs, _ = vqgan(images, n_rows=n_rows, n_cols=n_cols)
+    fake_disc_out = disc(fake_imgs)
 
     y_hat = torch.cat([real_img_disc_out, fake_disc_out])
     y_true = torch.cat([torch.ones_like(real_img_disc_out), torch.zeros_like(fake_disc_out)])
@@ -48,9 +54,9 @@ def run(train_ds, test_ds, vqgan, disc, vqgan_opt, disc_opt, N_STEPS, device):
     vqgan_opt.zero_grad()
     disc_opt.zero_grad()
     
-    fake_img, q_loss = vqgan(batch)
-    disc_out = disc(fake_img).sigmoid()
-    gen_loss = F.binary_cross_entropy(disc_out, torch.ones_like(disc_out)) + F.mse_loss(fake_img, batch) + q_loss
+    fake_img, q_loss = vqgan(images, n_rows=n_rows, n_cols=n_cols)
+    disc_out = disc(fake_img)
+    gen_loss = F.binary_cross_entropy(disc_out, torch.ones_like(disc_out)) + F.mse_loss(fake_img, images) + q_loss
     gen_loss.backward()
 
     vqgan_opt.step()
@@ -64,11 +70,11 @@ def run(train_ds, test_ds, vqgan, disc, vqgan_opt, disc_opt, N_STEPS, device):
     if i % 1000 == 0:
 
       vqgan.eval()
-      img, _ = vqgan(test_samples)
+      img, _ = vqgan(test_images, n_rows=test_n_rows, n_cols=test_n_cols)
       vqgan.train()
       img = img.detach().cpu().numpy()
       img = img.reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1)
-      n_cols = int(math.sqrt(img.shape[0])) + 1
+      _n_cols = int(math.sqrt(img.shape[0])) + 1
       fig, axs = plt.subplots(n_cols, n_cols)
       for j, ax in enumerate(axs.flatten()):
         if j < img.shape[0]:

main.py
@@ -1,9 +1,12 @@
+import dataclasses
 import os
 import torch
 
 import engine
 from dataset import DatasetLoaderLite
-from model import Encoder, Generator, Discriminator, Codebook, VQGAN
+from model import (Encoder, Generator, Discriminator, Codebook, VQGAN,
+  EncoderConfig, GeneratorConfig, DiscriminatorConfig, CodebookConfig
+)
 
 # ===
 # Constants
@@ -21,21 +24,52 @@ os.makedirs(MODEL_DIR, exist_ok=True)
 # ===
 # Intialization
 # ===
-train_ds = DatasetLoaderLite(train=True, root='data', batch_size=BATCH_SIZE, shuffle=True)
-test_ds = DatasetLoaderLite(train=False, root='data', batch_size=BATCH_SIZE, shuffle=False)
+patch_size = 4
+train_ds = DatasetLoaderLite(train=True, root='data', batch_size=BATCH_SIZE, shuffle=True, patch_size=patch_size)
+test_ds = DatasetLoaderLite(train=False, root='data', batch_size=BATCH_SIZE, shuffle=False, patch_size=patch_size)
 
-num_embeddings = 10
-input_dim = 32*32*3
-latent_dim = 128
+num_embeddings = 128
+input_dim = patch_size * patch_size * 3
 n_heads = 8
-embed_dim = latent_dim // n_heads
+embed_dim = 128
 
-codebook = Codebook(num_embeddings=num_embeddings, embedding_dim=embed_dim, n_heads=n_heads)
-encoder = Encoder(input_dim, latent_dim)
-generator = Generator(latent_dim, input_dim)
+ENCODER_CONFIG = EncoderConfig(
+  input_dim=input_dim,
+  max_len=32,
+  n_hidden_layers=4,
+  n_hidden_dims=[embed_dim, embed_dim, embed_dim*2, embed_dim*2],
+  merge_after=2,
+  intermediate_scale=2,
+  n_heads=n_heads
+)
+GENERATOR_CONFIG = GeneratorConfig(
+  input_dim=input_dim,
+  max_len=32,
+  n_hidden_layers=4,
+  n_hidden_dims=[embed_dim*2, embed_dim*2, embed_dim, embed_dim],
+  merge_after=2,
+  intermediate_scale=2,
+  n_heads=n_heads
+)
+DISCRIMINATOR_CONFIG = DiscriminatorConfig(
+  input_dim=input_dim,
+  max_len=32,
+  n_hidden_layers=4,
+  embed_dim=embed_dim,
+  intermediate_scale=2,
+  n_heads=n_heads
+)
+CODEBOOK_CONFIG = CodebookConfig(
+  num_embeddings=num_embeddings,
+  embedding_dim=ENCODER_CONFIG.n_hidden_dims[-1],
+)
+
+codebook = Codebook(**dataclasses.asdict(CODEBOOK_CONFIG))
+encoder = Encoder(**dataclasses.asdict(ENCODER_CONFIG))
+generator = Generator(**dataclasses.asdict(GENERATOR_CONFIG))
 vqgan = VQGAN(encoder, codebook, generator)
 
-discriminator = Discriminator(input_dim)
+discriminator = Discriminator(**dataclasses.asdict(DISCRIMINATOR_CONFIG))
 
 vqgan_opt = torch.optim.AdamW(vqgan.parameters(), lr=GEN_LR)
 disc_opt = torch.optim.AdamW(discriminator.parameters(), lr=DISC_LR)

model.py
@@ -1,168 +1,224 @@
+import dataclasses
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# upsample
-class UpSample(nn.Module):
-  def __init__(self, in_channels):
+
+@dataclasses.dataclass
+class EncoderConfig:
+  input_dim: int
+  max_len: int
+  n_hidden_layers: int
+  n_hidden_dims: list
+  merge_after: int
+  intermediate_scale: int
+  n_heads: int
+
+@dataclasses.dataclass
+class GeneratorConfig:
+  input_dim: int
+  max_len: int
+  n_hidden_layers: int
+  n_hidden_dims: list
+  merge_after: int
+  intermediate_scale: int
+  n_heads: int
+
+@dataclasses.dataclass
+class DiscriminatorConfig:
+  input_dim: int
+  max_len: int
+  n_hidden_layers: int
+  embed_dim: int
+  intermediate_scale: int
+  n_heads: int
+
+@dataclasses.dataclass
+class CodebookConfig:
+  num_embeddings: int
+  embedding_dim: int
+
+# TODO (rohan): add dropouts
+
+class AttentionBlock(nn.Module):
+  def __init__(self, embed_dim, n_heads):
     super().__init__()
-    self.conv = nn.ConvTranspose2d(in_channels, in_channels, kernel_size=3)
+    assert embed_dim % n_heads == 0, "Embedding dimension must be divisible by number of heads"
+    self.embed_dim = embed_dim
+    self.n_heads = n_heads
+    self.head_dim = embed_dim // n_heads
+    self.q = nn.Linear(embed_dim, embed_dim, bias=True)
+    self.k = nn.Linear(embed_dim, embed_dim, bias=True)
+    self.v = nn.Linear(embed_dim, embed_dim, bias=True)
+    self.out = nn.Linear(embed_dim, embed_dim, bias=True)
 
   def forward(self, x):
-    x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True)
-    return self.conv(x)
+    batch_size = x.size(0)
+    q = F.gelu(self.q(x)).view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+    k = F.gelu(self.k(x)).view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+    v = F.gelu(self.v(x)).view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
 
-# downsample
-class DownSample(nn.Module):
-  def __init__(self, in_channels):
+    attn = torch.einsum('bhid,bhjd->bhij', q, k) / self.head_dim**0.5
+    attn = F.softmax(attn, dim=-1)
+    x = torch.einsum('bhij,bhjd->bhid', attn, v).permute(0, 2, 1, 3).reshape(batch_size, -1, self.embed_dim)
+    x = F.gelu(self.out(x))
+    return x
+
+class MLP(nn.Module):
+  def __init__(self, embed_dim, intermediate_dim):
     super().__init__()
-    self.conv = nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1, stride=2)
+    self.fc1 = nn.Linear(embed_dim, intermediate_dim)
+    self.fc2 = nn.Linear(intermediate_dim, embed_dim)
+
   def forward(self, x):
-    return self.conv(x)
+    return self.fc2(F.gelu(self.fc1(x)))
+
+class EncoderBlock(nn.Module):
+  def __init__(self, embed_dim, intermediate_dim, n_heads):
+    super().__init__()
+    self.attn_block = AttentionBlock(embed_dim, n_heads)
+    self.mlp = MLP(embed_dim, intermediate_dim)
+    self.ln1 = nn.LayerNorm(embed_dim)
+    self.ln2 = nn.LayerNorm(embed_dim)
+
+  def forward(self, x):
+    x = x + self.attn_block(self.ln1(x))
+    x = x + self.mlp(self.ln2(x))
+    return x
+
+class PatchMerge(nn.Module):
+  def __init__(self, embed_dim):
+    super().__init__()
+    self.fc = nn.Linear(4*embed_dim, 2*embed_dim)
+
+  def forward(self, x, n_rows, n_cols):
+    batch_size, n_patches, embed_dim = x.size()
+    x = x.view(batch_size, n_rows, n_cols, embed_dim)
+    tl = x[:, ::2, ::2, :]
+    tr = x[:, ::2, 1::2, :]
+    bl = x[:, 1::2, ::2, :]
+    br = x[:, 1::2, 1::2, :]
+    x = torch.cat([tl, tr, bl, br], dim=-1)
+    x = x.view(batch_size, -1, 4*embed_dim)
+    x = self.fc(x)
+    return x, n_rows//2, n_cols//2
 
 
-# Encoder
+
 class Encoder(nn.Module):
-  def __init__(self, input_dim: int, output_dim: int):
-    super(Encoder, self).__init__()
-    dropout_rate = 0.1
-    self.layers = nn.Sequential(
-      nn.Linear(input_dim, 512),
-      nn.BatchNorm1d(512),
-      nn.ReLU(),
-      nn.Dropout(dropout_rate),
-      nn.Linear(512, 256),
-      nn.BatchNorm1d(256),
-      nn.ReLU(),
-      nn.Dropout(dropout_rate),
-      nn.Linear(256, 128),
-      nn.BatchNorm1d(128),
-      nn.ReLU(),
-      nn.Dropout(dropout_rate),
-      nn.Linear(128, 64),
-      nn.BatchNorm1d(64),
-      nn.ReLU(),
-      nn.Dropout(dropout_rate),
-      nn.Linear(64, 32),
-      nn.BatchNorm1d(32),
-      nn.ReLU(),
-      nn.Dropout(dropout_rate),
-      nn.Linear(32, 16),
-      nn.BatchNorm1d(16),
-      nn.ReLU(),
-      nn.Dropout(dropout_rate),
-      nn.Linear(16, 8),
-      nn.BatchNorm1d(8),
-      nn.ReLU(),
-      nn.Dropout(dropout_rate),
-      nn.Linear(8, output_dim)
-    )
+  def __init__(self, input_dim, max_len, n_hidden_layers, n_hidden_dims, merge_after, intermediate_scale, n_heads):
+    super().__init__()
 
-  def forward(self, x):
-    return self.layers(x)
+    self.patch_embedder = nn.Linear(input_dim, n_hidden_dims[0], bias=False)
+    self.position_embedder = nn.Embedding(max_len, n_hidden_dims[0])
+    self.blocks = nn.ModuleList([])
+    for i in range(n_hidden_layers):
+      self.blocks.append(EncoderBlock(n_hidden_dims[i], int(n_hidden_dims[i]*intermediate_scale), n_heads))
+      if (i+1) % merge_after == 0 and (i+1) != n_hidden_layers:
+        self.blocks.append(PatchMerge(n_hidden_dims[i]))
+
+  def forward(self, x, n_rows, n_cols):
+    x = self.patch_embedder(x)
+    x += self.position_embedder(torch.arange(x.size(1), device=x.device))
+    for block in self.blocks:
+      if block._get_name() == 'PatchMerge':
+        x, n_rows, n_cols = block(x, n_rows, n_cols)
+      else:
+        x = block(x)
+    return x, n_rows, n_cols
 
 
-# Generator
+class PatchUnMerge(nn.Module):
+  def __init__(self, embed_dim):
+    super().__init__()
+    assert embed_dim % 4 == 0, "Embedding dimension must be divisible by 4"
+    self.fc = nn.Linear(embed_dim // 4, embed_dim // 2)
+
+  def forward(self, x, n_rows, n_cols):
+    batch_size, n_patches, embed_dim = x.size()
+    x = x.view(batch_size, n_rows, n_cols, 2, 2, embed_dim // 4)
+    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(batch_size, n_rows*2, n_cols*2, embed_dim // 4).flatten(1, 2)
+    x = self.fc(x)
+    return x, n_rows*2, n_cols*2
+    
+
 class Generator(nn.Module):
-  def __init__(self, latent_dim: int, output_dim: int):
-    super(Generator, self).__init__()
-    self.layers = nn.Sequential(
-      nn.Linear(latent_dim, 128),
-      nn.BatchNorm1d(128),
-      nn.GELU(),
-      nn.Linear(128, 256),
-      nn.BatchNorm1d(256),
-      nn.GELU(),
-      nn.Linear(256, 512),
-      nn.BatchNorm1d(512),
-      nn.GELU(),
-      nn.Linear(512, 1024),
-      nn.BatchNorm1d(1024),
-      nn.GELU(),
-      nn.Linear(1024, 2048),
-      nn.BatchNorm1d(2048),
-      nn.GELU(),
-      nn.Linear(2048, 2048),
-      nn.BatchNorm1d(2048),
-      nn.GELU(),
-      nn.Linear(2048, 1024),
-      nn.BatchNorm1d(1024),
-      nn.GELU(),
-      nn.Linear(1024, 512),
-      nn.BatchNorm1d(512),
-      nn.GELU(),
-      nn.Linear(512, output_dim),
-      nn.Sigmoid()
-    )
+  def __init__(self, input_dim, max_len, n_hidden_layers, n_hidden_dims, merge_after, intermediate_scale, n_heads):
+    super().__init__()
 
-  def forward(self, x):
-    return self.layers(x)
+    self.blocks = nn.ModuleList([])
+    for i in range(n_hidden_layers):
+      self.blocks.append(EncoderBlock(n_hidden_dims[i], int(n_hidden_dims[i]*intermediate_scale), n_heads))
+      if (i+1) % merge_after == 0 and (i+1) != n_hidden_layers:
+        self.blocks.append(PatchUnMerge(n_hidden_dims[i]))
+    self.fc = nn.Linear(n_hidden_dims[-1], input_dim)
+
+  def forward(self, x, n_rows, n_cols):
+    for block in self.blocks:
+      if block._get_name() == 'PatchUnMerge':
+        x, n_rows, n_cols = block(x, n_rows, n_cols)
+      else:
+        x = block(x)
+    x = self.fc(x).sigmoid()
+    return x, n_rows, n_cols
 
 
-# Discriminator
 class Discriminator(nn.Module):
-  def __init__(self, input_dim: int):
-    super(Discriminator, self).__init__()
-    dropout_rate = 0.3
-    self.layers = nn.Sequential(
-      nn.Linear(input_dim, 512),
-      nn.ReLU(),
-      nn.Dropout(dropout_rate),
-      nn.Linear(512, 256),
-      nn.ReLU(),
-      nn.Dropout(dropout_rate),
-      nn.Linear(256, 128),
-      nn.ReLU(),
-      nn.Dropout(dropout_rate),
-      nn.Linear(128, 1),
-    )
+  def __init__(self, input_dim, max_len, n_hidden_layers, embed_dim, intermediate_scale, n_heads):
+    super().__init__()
+
+    self.patch_embedder = nn.Linear(input_dim, embed_dim, bias=False)
+    self.position_embedder = nn.Embedding(max_len, embed_dim)
+    self.blocks = nn.ModuleList([])
+    for i in range(n_hidden_layers):
+      self.blocks.append(EncoderBlock(embed_dim, int(embed_dim*intermediate_scale), n_heads))
+
+    self.out = nn.Linear(embed_dim, 1)
 
   def forward(self, x):
-    return self.layers(x)
+    x = self.patch_embedder(x)
+    x += self.position_embedder(torch.arange(x.size(1), device=x.device))
+    for block in self.blocks:
+      x = block(x)
+    x = self.out(x)
+    return x
+    
 
 
-# codebook
 class Codebook(nn.Module):
-  def __init__(self, num_embeddings, embedding_dim, n_heads):
-    super(Codebook, self).__init__()
+  def __init__(self, num_embeddings, embedding_dim):
+    super().__init__()
     self.embedding = nn.Embedding(num_embeddings, embedding_dim)
     self.num_embeddings = num_embeddings
     self.embedding_dim = embedding_dim
-    self.n_heads = n_heads
 
   def forward(self, x):
-    batch_size, embd_dim = x.shape
-
-    assert embd_dim % self.n_heads == 0, "embedding dim must be divisible by n_heads"
-    x = x.view(batch_size, self.n_heads, embd_dim // self.n_heads)
-
+    batch_size, seq_len, embd_dim = x.shape
     # calculate euclidean distance between x and each embedding
     dist = torch.norm(x.unsqueeze(-2) - self.embedding.weight, dim=-1)
-    indices = torch.argmin(dist, dim=-1).unsqueeze(-1)  # shape = (batch_size, n_heads, 1)
+    indices = torch.argmin(dist, dim=-1).unsqueeze(-1)  # shape = (batch_size, seq_len, 1)
     # create ohe vector with indices == 1 and other == 0
-    ohe = torch.zeros((batch_size, self.n_heads, self.num_embeddings), device=x.device)
+    ohe = torch.zeros((batch_size, seq_len, self.num_embeddings), device=x.device)
     ohe.scatter_(-1, indices, 1)
     ohe = ohe.unsqueeze(-1).expand(-1, -1, -1, self.embedding_dim)
     out = (ohe * self.embedding.weight).sum(dim=-2)
     # to let the grads flow
     out = x + out - x.detach()
-
-    out = out.view(batch_size, embd_dim)
     return out
 
 
 # compiling the enc-codebook-gen models into a single model
 class VQGAN(nn.Module):
   def __init__(self, encoder, codebook, generator):
-    super(VQGAN, self).__init__()
+    super().__init__()
     self.encoder = encoder
     self.codebook = codebook
     self.generator = generator
 
-  def forward(self, inp):
-    enc_inp = self.encoder(inp)
+  def forward(self, inp, n_rows, n_cols):
+    enc_inp, n_rows, n_cols = self.encoder(inp, n_rows, n_cols)
     tok_inp = self.codebook(enc_inp)
-    fake_img = self.generator(tok_inp)
+    fake_img, n_rows, n_cols = self.generator(tok_inp, n_rows, n_cols)
 
     # loss calculation
     sg_enc_inp = enc_inp.detach()