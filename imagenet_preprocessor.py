966b58fc4fc57d2c946d1cc29f6942e138446b78
Rohan Awhad <rohanavad007@gmail.com>
2024-06-19 12:06:25 -0700

    added save dir; tqdm

--- imagenet_preprocessor.py
+++ imagenet_preprocessor.py
@@ -1,9 +1,11 @@
+import os
 import pickle
 import random
 import torch
 
 from datasets import load_dataset
 from torchvision.transforms import v2
+from tqdm import tqdm
 
 def save_pickle(obj, path):
   with open(path, 'wb') as f:
@@ -17,14 +19,18 @@ TRANSFORM = v2.Compose([
 train_ds = load_dataset('ILSVRC/imagenet-1k', split='train')
 test_ds = load_dataset('ILSVRC/imagenet-1k', split='test')
 
-random.shuffle(train_ds)
+train_indices = list(range(len(train_ds)))
+random.shuffle(train_indices)
 
 # do train_ds
+SAVE_DIR = "/scratch/rawhad/datasets/preprocessed_imagenet"
+os.makedirs(SAVE_DIR, exist_ok=True)
+
 SHARD_SIZE = 2000
 SHARD_CNT = 0
 imgs = []
-for x in train_ds:
-  img = x['image']
+for idx in tqdm(train_indices):
+  img = train_ds[idx]['image']
   w, h = img.size
   new_w = min(w, h)
   # center crop
@@ -35,7 +41,7 @@ for x in train_ds:
   if len(imgs) >= SHARD_SIZE:
     # save the images
     imgs = torch.stack(imgs).numpy()
-    save_pickle(imgs, f'train_imgs_{SHARD_CNT:4d}.pkl')
+    save_pickle(imgs, os.path.join(SAVE_DIR, f'train_imgs_{SHARD_CNT:4d}.pkl'))
 
     # reset
     imgs = []
@@ -44,4 +50,4 @@ for x in train_ds:
 if len(imgs) > 0:
   # save the images
   imgs = torch.stack(imgs).numpy()
-  save_pickle(imgs, f'train_imgs_{SHARD_CNT:4d}.pkl')
+  save_pickle(imgs, os.path.join(SAVE_DIR, f'train_imgs_{SHARD_CNT:4d}.pkl'))
