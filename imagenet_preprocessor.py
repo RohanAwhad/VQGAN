6181eecf582361e7cbd28ebc5bf2893d50af5f89
Rohan Awhad <rohanavad007@gmail.com>
2024-06-20 13:58:53 -0700

    add multiprocessing

--- imagenet_preprocessor.py
+++ imagenet_preprocessor.py
@@ -1,3 +1,4 @@
+import multiprocessing as mp
 import os
 import pickle
 import random
@@ -40,33 +41,41 @@ train_indices = list(range(len(train_ds)))
 random.shuffle(train_indices)
 test_indices = list(range(len(test_ds)))
 
+def process(img: Image.Image):
+  img = img.convert('RGB')
+  w, h = img.size
+  new_w = min(w, h)
+  # center crop
+  img = v2.functional.center_crop(img, (new_w, new_w))
+  img_tensor = TRANSFORM(img) / 255.0
+
 
 def process_ds(indices, ds, split):
-  imgs = []
-  shard_id = 0
-  for idx in tqdm(indices):
-    img: Image.Image = ds[idx]['image']
-    w, h = img.size
-    new_w = min(w, h)
-    # center crop
-    img = v2.functional.center_crop(img, (new_w, new_w))
-    img_tensor = TRANSFORM(img) / 255.0
-    imgs.append(img_tensor)
+  n_procs = os.cpu_count() - 1
+  with mp.Pool(n_procs) as pool:
+    imgs = []
+    shard_id = 0
+    pbar = tqdm(total=len(indices), desc=f'Processing {split}')
+    for img_tensor in pool.imap_unordered(process, map(lambda idx: ds[idx], indices), chunksize=50):
+      pbar.update(1)
+      imgs.append(img_tensor)
 
-    if len(imgs) >= SHARD_SIZE:
+      if len(imgs) >= SHARD_SIZE:
+        # save the images
+        imgs = torch.stack(imgs).numpy()
+        save_pickle(imgs, os.path.join(SAVE_DIR, f'{split}_{shard_id}.pkl'))
+
+        # reset
+        imgs = []
+        shard_id += 1
+
+    if len(imgs) > 0:
       # save the images
       imgs = torch.stack(imgs).numpy()
-      save_pickle(imgs, os.path.join(SAVE_DIR, f'{split}_{shard_id:4d}.pkl'))
-
-      # reset
-      imgs = []
-      shard_id += 1
-
-  if len(imgs) > 0:
-    # save the images
-    imgs = torch.stack(imgs).numpy()
-    save_pickle(imgs, os.path.join(SAVE_DIR, f'{split}_{shard_id:4d}.pkl'))
+      save_pickle(imgs, os.path.join(SAVE_DIR, f'{split}_{shard_id}.pkl'))
 
 
-process_ds(train_indices, train_ds, 'train')
-process_ds(test_indices, test_ds, 'test')
+
+if __name__ == '__main__':
+  process_ds(train_indices, train_ds, 'train')
+  process_ds(test_indices, test_ds, 'test')
