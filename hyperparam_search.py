489c9c36f12aea90878ca070597486b0447fd782
Rohan Awhad <rohanavad007@gmail.com>
2024-06-20 15:04:00 -0700

    updated to allow for hyperparam search

---
+++ hyperparam_search.py
@@ -0,0 +1,51 @@
+# Hyperparams: LR and DROPOUT_RATE
+SLURM_JOBSCRIPT_TMPLT = """#!/bin/bash
+
+#SBATCH -N 1            # number of nodes
+#SBATCH -c 4            # number of cores 
+#SBATCH -t 0-00:30:00   # time in d-hh:mm:ss
+#SBATCH --mem=10G
+#SBATCH -G a30:1
+#SBATCH -p general      # partition 
+#SBATCH -q public       # QOS
+#SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
+#SBATCH --export=NONE   # Purge the job-submitting shell environment
+
+# Load required modules for job's environment
+module load mamba/latest
+
+# Using python, so source activate an appropriate environment
+source activate interest_region_cls
+echo $LD_LIBRARY_PATH
+nvidia-smi
+source "/home/rawhad/personal_jobs/VQGAN/dev.env"
+
+python -c "import torch; print(torch.cuda.is_available())"
+python "/home/rawhad/personal_jobs/VQGAN/VQGAN/main.py \
+  --lr {lr} \
+  --batch_size 64 \
+  --n_steps 500 \
+  --num_embeddings 1024 \
+  --dropout_rate {dropout_rate} \
+  --data_dir "/scratch/rawhad/datasets/preprocessed_tiny_imagenet" \
+  --project_name vqgan_hyperparam_search \
+  --run_name {run_name}
+"""
+
+
+import random
+import os
+
+ATTEMPT = 0
+# N = 20
+N = 2
+for i in range(N):
+  lr = 10**random.uniform(-5, -2)
+  dropout_rate = 10**random.uniform(-2, -0.8)
+  run_name = f'run-{ATTEMPT}-{i}'
+  with open(f'hyperparam_scripts/{run_name}.sh', 'w') as f:
+    f.write(SLURM_JOBSCRIPT_TMPLT.format(lr=lr, dropout_rate=dropout_rate, run_name=run_name))
+
+  os.system(f'sbatch hyperparam_scripts/{run_name}.sh')
--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 from dotenv import load_dotenv
 load_dotenv('/home/rawhad/personal_jobs/VQGAN/dev.env')
 
+import argparse
 import os
 import torch
 
 import engine, logger
-from dataset import DatasetLoaderLite
+from dataset import ImageNetDatasetLoaderLite
 from model import Encoder, Generator, Discriminator, Codebook, VQGAN
 
 # ===
 # Constants
 # ===
-GEN_LR = 3e-4
-DISC_LR = 3e-4
-BATCH_SIZE = 64
-N_STEPS = 5000
+argparser = argparse.ArgumentParser()
+argparser.add_argument('--lr', type=float, default=3e-4)
+argparser.add_argument('--batch_size', type=int, default=64)
+argparser.add_argument('--n_steps', type=int, default=5000)
+argparser.add_argument('--num_embeddings', type=int, default=1024)
+argparser.add_argument('--dropout_rate', type=float, default=0.1)
+argparser.add_argument('--data_dir', type=str, default="/scratch/rawhad/datasets/preprocessed_tiny_imagenet")
+argparser.add_argument('--save_model', type=bool, default=True)
+argparser.add_argument('--project_name', type=str, default='vqgan')
+argparser.add_argument('--run_name', type=str, default='test-imagenet')
+args = argparser.parse_args()
+
+LR = args.lr
+BATCH_SIZE = args.batch_size
+N_STEPS = args.n_steps
+NUM_EMBEDDINGS = args.num_embeddings
+DROPOUT_RATE = args.dropout_rate
+DATA_DIR = args.data_dir
+SAVE_MODEL = args.save_model
+PROJECT_NAME = args.project_name
+RUN_NAME = args.run_name
+
+GEN_LR = LR
+DISC_LR = LR
 
 DEVICE = 'cpu'
 if torch.cuda.is_available(): DEVICE = 'cuda'
@@ -23,19 +44,19 @@ if torch.backends.mps.is_available(): DEVICE = 'mps'
 MODEL_DIR = './models'
 os.makedirs(MODEL_DIR, exist_ok=True)
 
-LOGGER = logger.WandbLogger(project_name='vqgan', run_name='test-imagenet')
+LOGGER = logger.WandbLogger(project_name=PROJECT_NAME, run_name=RUN_NAME)
 #LOGGER = logger.ConsoleLogger(project_name='vqgan', run_name='test-imagenet')
 
 # ===
 # Intialization
 # ===
-train_ds = DatasetLoaderLite(split='train', batch_size=BATCH_SIZE, shuffle=True)
-test_ds = DatasetLoaderLite(split='validation', batch_size=BATCH_SIZE, shuffle=False)
+train_ds = ImageNetDatasetLoaderLite(split='train', batch_size=BATCH_SIZE, root=DATA_DIR)
+test_ds = ImageNetDatasetLoaderLite(split='train', batch_size=BATCH_SIZE, root=DATA_DIR)
 
-codebook = Codebook(num_embeddings=64, embedding_dim=2048)
-encoder = Encoder()
-generator = Generator()
-discriminator = Discriminator()
+codebook = Codebook(num_embeddings=NUM_EMBEDDINGS, embedding_dim=2048)
+encoder = Encoder(DROPOUT_RATE)
+generator = Generator(DROPOUT_RATE)
+discriminator = Discriminator(DROPOUT_RATE)
 vqgan = VQGAN(encoder, codebook, generator)
 
 
@@ -54,6 +75,7 @@ codebook.to('cpu')
 encoder.to('cpu')
 generator.to('cpu')
 
-torch.save(codebook.state_dict(), os.path.join(MODEL_DIR, 'codebook.pth'))
-torch.save(encoder.state_dict(), os.path.join(MODEL_DIR, 'encoder.pth'))
-torch.save(generator.state_dict(), os.path.join(MODEL_DIR, 'generator.pth'))
+if SAVE_MODEL:
+  torch.save(codebook.state_dict(), os.path.join(MODEL_DIR, 'codebook.pth'))
+  torch.save(encoder.state_dict(), os.path.join(MODEL_DIR, 'encoder.pth'))
+  torch.save(generator.state_dict(), os.path.join(MODEL_DIR, 'generator.pth'))
