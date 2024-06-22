1e1c94bbd68173b4b3962b1971beb6037825766c
Rohan Awhad <rohanavad007@gmail.com>
2024-06-22 12:15:19 -0700

    added restart functionality

--- imagenet_preprocessor.py
+++ imagenet_preprocessor.py
@@ -1,3 +1,4 @@
+import glob
 import multiprocessing as mp
 import os
 import pickle
@@ -37,8 +38,16 @@ def save_pickle(obj, path):
 train_ds = load_dataset('ILSVRC/imagenet-1k', split='train')
 test_ds = load_dataset('ILSVRC/imagenet-1k', split='test')
 
-train_indices = list(range(len(train_ds)))
-random.shuffle(train_indices)
+TRAIN_INDICES_PATH = 'shuffled_train_indices.txt'
+if os.path.exists(TRAIN_INDICES_PATH):
+  with open(TRAIN_INDICES_PATH, 'r') as f:
+    train_indices = [int(x) for x in f.read().split('\n') if x]
+else:
+  train_indices = list(range(len(train_ds)))
+  random.shuffle(train_indices)
+  with open(TRAIN_INDICES_PATH, 'w') as f:
+    f.write('\n'.join([str(x) for x in train_indices]))
+
 test_indices = list(range(len(test_ds)))
 
 def process(img: Image.Image):
@@ -50,6 +59,9 @@ def process(img: Image.Image):
   img_tensor = TRANSFORM(img) / 255.0
   return img_tensor
 
+def restore(split):
+  files = glob.glob(f'{SAVE_DIR}/{split}_*.pkl')
+  return len(files)
 
 def process_ds(indices, ds, split):
   n_procs = max(1, os.cpu_count()//2)
@@ -58,6 +70,13 @@ def process_ds(indices, ds, split):
     imgs = []
     shard_id = 0
     pbar = tqdm(total=len(indices), desc=f'Processing {split}')
+    # restore
+    shard_id = restore(split)
+    n_processed = shard_id * SHARD_SIZE
+    indices = indices[n_processed:]
+    pbar.update(n_processed)
+
+
     for img_tensor in pool.imap_unordered(process, map(lambda idx: ds[idx]['image'], indices), chunksize=100):
       pbar.update(1)
       imgs.append(img_tensor)
